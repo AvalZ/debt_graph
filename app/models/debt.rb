1bb5c71dd6c4de351e4bc3b428411e50037956c6
avalz <avalenza89@gmail.com>
2016-04-20 23:24:01 +0200

    Add debts and credits calculation for participants.

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,4 +1,5 @@
 class Debt < ActiveRecord::Base
   belongs_to :from, class_name: "Participant"
   belongs_to :to, class_name: "Participant"
+
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,12 @@
 class Participant < ActiveRecord::Base
   belongs_to :group
-  has_many :debts
+
+  def debts
+    Debt.where(from: self).sum(:amount)
+  end
+
+  def credits
+    Debt.where(to: self).sum(:amount)
+  end
+
 end
